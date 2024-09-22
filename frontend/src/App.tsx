000c79bc07169202e6bea54c149478fe71d279ce
Eakjot Hundal <eakjothundal@gmail.com>
2024-09-22 02:45:51 -0700

    implement markdown viewer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,22 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import { Button } from "./components/ui/button";
+import Markdown from "react-markdown";
 
 function App() {
   const [data, setData] = useState(null);
 
-  useEffect(() => {
-    fetch(`${process.env.API}/api`)
+  const justDoIt = () => {
+    fetch(`http://localhost:3055/api`)
       .then((res) => res.json()) // Parse JSON
       .then((data) => setData(data.message)); // Set the message as data
-  }, []);
+  };
 
   return (
     <div>
+      <Button onClick={justDoIt}>Just Do It</Button>
+
       <h1>Message from backend:</h1>
-      {data ? <p>{data}</p> : <p>Loading...</p>}
+      {data ? <Markdown>{data}</Markdown> : <p>Loading...</p>}
     </div>
   );
 }
