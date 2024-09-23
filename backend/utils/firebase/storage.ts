3df4a76418b691090e072fcdd5048d0d9cce4e65
Eakjot Hundal <eakjothundal@gmail.com>
2024-09-23 00:57:46 -0700

    move firebaseConfig object to own file

--- backend/utils/firebase/storage.ts
+++ backend/utils/firebase/storage.ts
@@ -2,15 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getStorage, ref, uploadBytes } from "firebase/storage";
 import fs from "fs";
 
-const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  appId: process.env.FIREBASE_APP_ID,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
-};
+import { firebaseConfig } from "./config";
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
@@ -24,11 +16,8 @@ const testRef = ref(storage, "test.m4a");
 // Function to upload local file to Firebase Storage
 export const upload = async (file: File | Blob) => {
   try {
-    // Read the local file into a buffer
-    const fileBuffer = fs.readFileSync("../../testData/test.m4a");
-
     // Upload the buffer to Firebase Storage
-    await uploadBytes(testRef, fileBuffer);
+    await uploadBytes(testRef, file);
 
     console.log("File uploaded successfully");
   } catch (error) {
