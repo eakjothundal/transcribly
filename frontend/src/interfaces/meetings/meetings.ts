e41ce2fe7a6ce14b822db93e67292451b08d8bcc
Eakjot Hundal <eakjothundal@gmail.com>
2024-11-11 16:48:57 -0600

    add types

--- frontend/src/interfaces/meetings/meetings.ts
+++ frontend/src/interfaces/meetings/meetings.ts
@@ -7,3 +7,28 @@ export type MeetingSummaryOptions =
   | "next_steps"
   | "improvements"
   | "vibe";
+
+type ListField = {
+  value: string[];
+};
+
+export type Meeting = {
+  meeting_id: string;
+  user_id: string;
+  project_id: string;
+  template_id: string;
+  meeting_name: string;
+  meeting_date: string;
+  added_context: string;
+  transcript: string;
+  summary: string;
+  notes: ListField;
+  action_items: ListField;
+  key_topics: ListField;
+  decisions: ListField;
+  next_steps: ListField;
+  improvements: ListField;
+  vibe: string;
+  created_at: string;
+  updated_at: string;
+};
--- frontend/src/interfaces/templates/templates.ts
+++ frontend/src/interfaces/templates/templates.ts
@@ -1,5 +1,15 @@
 import { MeetingSummaryOptions } from "../meetings";
 
+export type Template = {
+  template_id: string;
+  user_id: string;
+  template_name: string;
+  template_definition: string;
+  template_settings: TemplateSettings;
+  created_at: string;
+  updated_at: string;
+};
+
 export type TemplateSetting = {
   description: string;
   enabled: boolean;
