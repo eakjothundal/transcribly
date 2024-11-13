d042a541af8872722d403fa938fe8fb0b87643f1
Eakjot Hundal <eakjothundal@gmail.com>
2024-11-12 19:38:20 -0600

    remove upload area from Home

--- frontend/src/components/home/Home.tsx
+++ frontend/src/components/home/Home.tsx
@@ -1,25 +1,18 @@
-import { UploadArea } from "./UploadArea";
-import { MarkdownViewer } from "./MarkdownViewer";
-import { AddProject } from "../Projects";
-import { AddTemplate } from "../Templates";
-import { NewMeeting } from "../Meetings";
+import { Stack } from "@mantine/core";
 
 import classes from "./Home.module.css";
 
-import { Stack } from "@mantine/core";
-import { useState } from "react";
+import { AddProject } from "../Projects";
+import { AddTemplate } from "../Templates";
+import { NewMeeting } from "../Meetings";
 import { LogoutButton } from "../Auth/LogoutButton/LogoutButton";
 
 export function Home() {
-  const [summary, setSummary] = useState<string | null>(null);
-
   return (
     <Stack align="center" className={classes.home}>
       <AddProject />
       <AddTemplate />
       <NewMeeting />
-      <UploadArea setSummary={setSummary} />
-      <MarkdownViewer summary={summary} />
       <LogoutButton />
     </Stack>
   );
