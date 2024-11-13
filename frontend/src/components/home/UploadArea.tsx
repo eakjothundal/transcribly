b08e89d7ef474bfe9741030881bcf92911df5d6a
Eakjot Hundal <eakjothundal@gmail.com>
2024-11-12 19:18:17 -0600

    remove templates dropdown and logic from UploadArea

--- frontend/src/components/home/UploadArea.tsx
+++ frontend/src/components/home/UploadArea.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Text, Select } from "@mantine/core";
+import { Box, Flex, Text } from "@mantine/core";
 import { Dropzone } from "@mantine/dropzone";
 
 import classes from "./Home.module.css";
@@ -6,28 +6,15 @@ import { useState } from "react";
 
 interface UploadAreaProps {
   setSummary: (summary: string) => void;
+  disabled?: boolean;
 }
 
-const templates = [
-  "Daily Huddle",
-  "Weekly Pulse",
-  "Bi-Weekly Check-In",
-  "Culture Chat",
-  "Quarterly",
-];
-
 export function UploadArea(props: UploadAreaProps) {
-  const { setSummary } = props;
+  const { setSummary, disabled } = props;
 
-  const [selectedTemplate, setSelectedTemplate] = useState<string>("");
   const [loading, setLoading] = useState<boolean>(false);
 
   const uploadFile = async (file: File, template: string) => {
-    if (!templates.includes(template)) {
-      console.error("Invalid template, check the logic and try again");
-      return;
-    }
-
     setLoading(true);
 
     const formData = new FormData();
@@ -49,25 +36,15 @@ export function UploadArea(props: UploadAreaProps) {
   };
 
   return (
-    <Box className={classes.uploadArea}>
-      <Box>
-        <Select
-          label="Choose your meeting type:"
-          data={templates}
-          defaultValue={"Daily Huddle"}
-          onChange={(selectedValue) =>
-            selectedValue && setSelectedTemplate(selectedValue)
-          }
-          searchable
-        />
-      </Box>
-
+    <Box>
       <Dropzone
-        onDrop={(files) => uploadFile(files[0], selectedTemplate)}
+        onDrop={(files) => uploadFile(files[0], "meeting")}
         onReject={(files) => console.log("rejected files", files)}
         maxFiles={1}
         maxSize={50 * 1024 ** 2}
         loading={loading}
+        disabled={disabled}
+        className={disabled ? classes.dropzoneDisabled : ""}
       >
         <Flex
           justify="center"
